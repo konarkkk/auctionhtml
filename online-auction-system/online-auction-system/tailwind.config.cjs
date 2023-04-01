61ee856d6ec3eaf1be59899958979d8b3b33e343
konark <lci2020050@iiitl.ac.in>
2023-04-01 12:02:41 +0530

    f-end

---
+++ online-auction-system/online-auction-system/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*"
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      alphaa:'#7190be',
+    },
+  },
+  plugins: [],
+}
